57d15a44eab1fa5da6c3a89701875a280fd683ef
Mojca Miklavec <mojca.miklavec@gmail.com>
2011-02-10 23:49:54 +0100

    fix random seed, add trasparent solids and filled curves, add frscript.ttf font

--- copy_demos.rb
+++ copy_demos.rb
@@ -20,10 +20,24 @@ class Demo
 			system "cp -p demo/original/#{dep} demo/tex/"
 		end
 	end
+	def copy_file
+		write_file(get_content)
+	end
 end
 
 circles = Demo.new('circles',
 	['world.dat','world.cor','energy_circles.dat','optimize.dat',
 	 'lena.rgb','lena-keypoints.bin','empty-circles.dat','delaunay-edges.dat','hemisphr.dat'])
-data = circles.get_content
-circles.write_file(data)
+circles.copy_file
+
+fillcrvs = Demo.new('fillcrvs', ['world.dat'])
+data = fillcrvs.get_content
+# data.gsub!(/(\(x>3.5 \? x\/3-3 : 1\/0\))/, '\1 t \'$(x>3.5?x/3-3:1/0)$\'')
+# data.scan(/\s+([^\s]+)\s+([^\s]+)\s+with/) do |t1,t2|
+# 	puts ">>#{t1}<<\n>>#{t2}<<\n\n"
+# end
+fillcrvs.write_file(data)
+
+transparent_solids = Demo.new('transparent_solids', [])
+data = transparent_solids.get_content
+transparent_solids.write_file(data)
